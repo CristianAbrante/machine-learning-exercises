d999612c26ab739b7a8f5f58455fb485188ef55a
Cristian Abrante <cristian@abrante.me>
2020-09-30 11:49:44 +0300

    Ex-2: errors calculated and plotted

--- assignment-2/main.py
+++ assignment-2/main.py
@@ -5,50 +5,14 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 
 if __name__ == '__main__':
-    n_tot = 50  # choose the number of samples to be generated
-
-    n = int(n_tot / 2)  # will use half in training, half in testing
-
-    # two blobs; labels are 0 and 1
-    X, y = make_blobs(n_tot, centers=2, cluster_std=4.0, random_state=1)
-
-    # divide into training and testing
-    np.random.seed(42)
-    order = np.random.permutation(n_tot)
-    train = order[:n]  # these will be the training samples
-    test = order[n:]  # and these are for testing
-
-    my_classifier = Perceptron()
-    # this is how to train the model with data in training set
-    my_classifier.fit(X[train, :], y[train])
-    # and this is how to predict the labels for test data
-    predictions = my_classifier.predict(X[test, :])
-
-    training_error = 1 - accuracy_score(y[test], predictions, normalize=False) / float(y[train].size)
-
-    x_train = X[train, :]
-    y_train = y[train]
-    x_test = X[test, :]
-    y_test = y[test]
-    test_predicted = predictions
-
-    # Plot training set
-    print(y_test)
-    print(test_predicted)
-
-    coincident = 0
-    for i in range(y_test.size):
-        if y_test[i] == test_predicted[i]:
-            coincident += 1
-
-    print("size -> ", y_test.size)
-    print("coincident -> ", coincident)
-    print("Not coincident -> ", y_test.size - coincident)
-    print("Error -> ", training_error)
-
     np.random.seed(42)
 
-    for n_tot in range(20, 200):
+    n_tots = range(20, 201)
+
+    training_errors = np.array([])
+    test_errors = np.array([])
+
+    for n_tot in n_tots:
         n = int(n_tot / 2)  # will use half in training, half in testing
 
         # two blobs; labels are 0 and 1
@@ -59,10 +23,31 @@ if __name__ == '__main__':
         train = order[:n]  # these will be the training samples
         test = order[n:]  # and these are for testing
 
-        my_classifier = Perceptron()
-        # this is how to train the model with data in training set
-        my_classifier.fit(X[train, :], y[train])
-        # and this is how to predict the labels for test data
-        predictions = my_classifier.predict(X[test, :])
+        x_train = X[train, :]
+        y_train = y[train]
+        x_test = X[test, :]
+        y_test = y[test]
 
-        training_error = 1 - accuracy_score(y[test], predictions, normalize=False) / float(y[train].size)
+        my_classifier = Perceptron()
+
+        # this is how to train the model with data in training set
+        my_classifier.fit(x_train, y_train)
+
+        # calculate predictions
+        y_pred_train = my_classifier.predict(x_train)
+        y_pred_test = my_classifier.predict(x_test)
+
+        # Calculate error for train and test
+        error_train = 1 - accuracy_score(y_train, y_pred_train, normalize=False) / float(y_train.size)
+        error_test = 1 - accuracy_score(y_test, y_pred_test, normalize=False) / float(y_test.size)
+
+        # Append to total errors
+        training_errors = np.append(training_errors, error_train)
+        test_errors = np.append(test_errors, error_test)
+
+    plt.plot(n_tots, training_errors, 'r', label="training error")
+    plt.plot(n_tots, test_errors, 'b', label="test error")
+    plt.xlabel("Number of data points")
+    plt.ylabel("Error")
+    plt.legend(loc='upper right', frameon=False)
+    plt.show()
